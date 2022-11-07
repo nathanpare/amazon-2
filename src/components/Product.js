90e47e4e4752b6968e8c1ffd72517c99297b8ffa
Nathan Pare <nathanpare@Nathans-MBP.lan>
2022-11-06 16:50:18 -0800

    Auth Setup

--- src/components/Product.js
+++ src/components/Product.js
@@ -20,7 +20,7 @@ const Product = ({ id, title, price, description, category, image }) => {
       <p className='absolute top-2 right-2 text-xs italic text-gray-400'>
 
         {category}
-        </p>
+      </p>
 
       <Image src={image} height={200} width={200} objectFit="contain" />
 
@@ -28,16 +28,16 @@ const Product = ({ id, title, price, description, category, image }) => {
 
       <div className='flex'>
         {Array(rating)
-        .fill()
-        .map((_, i) => (
-          <StarIcon className='h-5 text-yellow-500' />
-        ))}
+          .fill()
+          .map((_, i) => (
+            <StarIcon className='h-5 text-yellow-500' />
+          ))}
       </div>
 
       <p className='text-xs my-2 line-clamp-2'>{description}</p>
 
       <div className='mb-5'>
-          <CurrencyFormat value={price} displayType={'text'} thousandSeparator={true} prefix={"$"} />
+        <CurrencyFormat value={price} displayType={'text'} thousandSeparator={true} prefix={"$"} />
       </div>
 
       {hasPrime && (
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -23,17 +23,17 @@ const ProductFeed = ({ products }) => {
       />
 
       <div className='md:col-span-2'>
-      {products.slice(4, 5).map(({ id, title, price, description, category, image }) => (
-        <Product
-          key={id}
-          id={id}
-          title={title}
-          price={price}
-          description={description}
-          category={category}
-          image={image}
-        />
-      ))}
+        {products.slice(4, 5).map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
       </div>
 
       {products.slice(5, products.length).map(({ id, title, price, description, category, image }) => (
