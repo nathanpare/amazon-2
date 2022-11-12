0db12239360088ba623faa490d2df79a1a723c97
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-11-12 12:32:45 -0800

    start building checkout product

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
 
-const CheckoutProduct = ({ 
+import { StarIcon } from '@heroicons/react/solid';
+import CurrencyFormat from 'react-currency-format';
+
+const CheckoutProduct = ({
   id,
   title,
   price,
@@ -10,12 +13,44 @@ const CheckoutProduct = ({
   category,
   image,
   hasPrime,
- }) => {
+}) => {
   return (
     <div className='grid grid-cols-5'>
       <Image src={image} height={200} width={200} objectFit="contain" />
+
+      <div className='col-span-3 mx-5'>
+        <p>{title}</p>
+
+        <div className='flex'>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <StarIcon key={i} className="h-5 text-yellow-500" />
+            ))}
+        </div>
+
+        <p className='text-xs my-2 line-clamp-3'>{description}</p>
+        <CurrencyFormat value={price} displayType={'text'} thousandSeparator={true} prefix={"$"} />
+
+        {hasPrime && (
+          <div className='flex items-center space-x-2'>
+            <img
+            loading='lazy'
+            className='w-12'
+            src='https://links.papareact.com/fdw'
+            alt=''
+            />
+            <p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
+          </div>
+        )}
+      </div>
+
+      <div className='flex flex-col space-y-2 my-auto justify-self-end'>
+        <button className='button'>Add to Cart</button>
+        <button className='button'>Remove from Cart</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CheckoutProduct;
--- src/components/Product.js
+++ src/components/Product.js
@@ -24,9 +24,11 @@ const Product = ({ id, title, price, description, category, image }) => {
       id,
       title,
       price,
+      rating,
       description,
       category,
       image,
+      hasPrime,
     };
 
     dispatch(addToBasket(product));
